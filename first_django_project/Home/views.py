e9e6f7582f9b65449172bcee850c752cf10025f3
masoomk9666 <saadk9666@gmail.com>
2025-10-24 07:49:31 -0700

    URL Dispatching Completed

--- first_django_project/Home/views.py
+++ first_django_project/Home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello this is Home Page')
+def about(request):
+    return HttpResponse('Hello this is About Page')
+def services(request):
+    return HttpResponse('Hello this is Services Page')
+def contact(request):
+    return HttpResponse('Hello this is Contact Page')
