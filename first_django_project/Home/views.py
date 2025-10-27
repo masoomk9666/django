b9c03893592fdb9edbf9766ff01d6390d6b69a0b
masoomk9666 <saadk9666@gmail.com>
2025-10-27 08:16:56 -0700

    POST Request Handled for COntact us Page and Also Used Django Messages for Events

--- first_django_project/Home/views.py
+++ first_django_project/Home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from Home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -18,4 +20,13 @@ def skills(request):
 def experience(request):
         return render(request, "experience.html")
 def contact(request):
+        if request.method == "POST":
+               name = request.POST.get('name')
+               email = request.POST.get('email')
+               phone = request.POST.get('phone')
+               message = request.POST.get('message')
+               contact = Contact(name=name, email=email, phone=phone, message=message)
+               contact.save()
+               messages.success(request, "Your Form Submission Succesful.")
+               
         return render(request, "contact.html")
